531a94271b4d11915bb3cd582e32fd124ef58037
lfu456 <109532742+lfu456@users.noreply.github.com>
2023-04-04 02:05:44 +0700

    xoa bien
    const experience = new Experience(document.querySelector('.experience-canvas'))

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 import Experience from './src/Experience/Experience'
 
-const experience = new Experience(document.querySelector('.experience-canvas'))
 
 
